29c75a3d2706ad3aab7020ce9ee6f76bd34bfea7
aht-taiht <79570355+aht-taiht@users.noreply.github.com>
2024-06-04 16:47:54 +0700

    Update account_account.py

--- onnet_account_type_mapping/models/account_account.py
+++ onnet_account_type_mapping/models/account_account.py
@@ -2,7 +2,7 @@ from odoo import fields, models, api
 from odoo.exceptions import ValidationError, UserError
 
 class AccountAccount(models.Model):
-    _name = 'account.account'
+    _inherit = 'account.account'
 
-    # user_type_id = fields.Many2one('account.account.type', string='Type', required=True)
-    # type = fields.Selection(related='user_type_id.type', string='Loại gốc', readonly=True)
+    user_type_id = fields.Many2one('account.account.type', string='Type', required=True)
+    type = fields.Selection(related='user_type_id.type', string='Loại gốc', readonly=True)
